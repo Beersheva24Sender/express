087c2d876568850afb14b101c085bb15200c56e6
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-02-10 16:02:41 +0200

    add Joi validation for course creation and update endpoints

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-import express from 'express'
+import express from 'express';
+import Joi from 'joi';
+
+const schemaPost = Joi.object({
+    "id": Joi.string().alphanum().pattern(/^J\d{3}/).required(),
+    "name": Joi.string().valid("Front-End", "JAVA", "Back-End", "Node", "AWS", "C++").required(),
+    "description": Joi.string().required(),
+})
+
+const schemaPut = Joi.object({
+    "id": Joi.string().alphanum().pattern(/^J\d{3}/),
+    "name": Joi.string().valid("Front-End", "JAVA", "Back-End", "Node", "AWS", "C++"),
+    "description": Joi.string(),
+})
+
 const courses = {
     123: { id: '123', name: 'node', description: 'node description' },
     456: { id: '456', name: 'express', description: 'express description' },
     789: { id: '789', name: 'mongodb', description: 'mongodb description' },
 };
+
 const app = express();
 const port = process.env.PORT || 3500;
 app.use(express.json());
 
 app.post('/api/v1/courses', (req, res) => {
-    const id = req.body.id;
-    courses[id] = req.body;
-    res.status(201).send(courses.id);
+
+
+
+    const { error } = schemaPost.validate(req.body, { abortEarly: false });
+    if (error) {
+        return res.status(400).send(error.details.map(d => d.message).join(";"));
+    } else {
+        const id = req.body.id;
+        courses[id] = req.body;
+        res.status(201).send(courses.id);
+    }
 });
 
 app.get('/api/v1/courses/:id', (req, res) => {
@@ -33,6 +56,6 @@ app.get('/api/v1/courses', (req, res) => {
     const { name, description } = req.query;
     res.send(Object.values(courses).filter(c => (name ? c.name === name : true)
         && (description ? c.description === description : true)))
-    });
+});
 
 app.listen(port, () => console.log(`server is listening on ${port}`));
