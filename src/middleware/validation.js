212b2859cf4918ed0d785aa456f7c0e7565f7634
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-02-12 14:40:56 +0200

    add validation middleware for request body validation and error handling

---
+++ src/middleware/validation.js
@@ -0,0 +1,35 @@
+import { createError } from "../errors/errors.js";
+
+export function expressValidator(schemasObj) {
+    return (req, res, next) => {
+        if (req._body) {
+            const { error } = schemasObj[req.method].validate(req.body, {
+                abortEarly: false,
+            });
+            if (error) {
+                req.errorMessage = error.details.map((d) => d.message).join(";");
+            }
+            req.validate = true;
+        };
+        next();
+    }
+}
+
+export function validator(schema) {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            throw createError(400, error.details.map((d) => d.message).join(";"));
+        }
+        next();
+    };
+}
+export function valid(req, res, next) {
+    if (!req.validated) {
+        throw createError(500, "server has not validated request");
+    }
+    if (req.errorMessage) {
+        throw createError(400, req.errorMessage);
+    }
+    next();
+}
