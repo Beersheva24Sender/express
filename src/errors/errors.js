cc360dd20ff0a49a5aa7af59842ea0a31d02402e
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-03-03 17:46:01 +0200

    fix typo in error handler function name

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -2,7 +2,7 @@ export function createError(status, message) {
     return { status, message };
 }
 
-export function errorHnalder(error, req, res, next) {
+export function errorHandler(error, req, res, next) {
     let { status, message } = error;
     status = status ?? 500;
     message = message ?? "internal error server" + error
